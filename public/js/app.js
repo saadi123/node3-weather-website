f475b6882bf4bfd14abbf6c71ebe7b5791179c65
Saad <saadpaki@gmail.com>
2020-05-27 01:11:14 +0500

    Amended .js File

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 fetch('http://puzzle.mead.io/puzzle').then((response)=>{
         response.json().then((data)=>{
                 console.log(data);
-            })
+            });
     });
 
 fetch('http://localhost:3000/weather?address=boston').then((response)=>{
